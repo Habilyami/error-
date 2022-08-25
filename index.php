39e00c19b3c2f4fba5ecbefa767a8dd42413ff5c
Habil <labaikaabubakar6@gmail.com>
2022-08-25 17:01:35 +0100

    error page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Error Page</title>
+</head>
+<body>
+    <main>
+        <section>
+            <img src="../error/images/404.png">
+            <h1>page not found</h1>
+            <p>The page you are looking for was moved, removed, renmaed or might never existed.</p>
+        </section>
+        <fieldset>
+            <button class="go">go home</button>
+            <button class="us">contact us</button>
+        </fieldset>
+    </main>
+</body>
+</html>
+<?php
+?>
+
